14912bdfe16ecbd3b6c6e2e1a784460e5e3518ec
alvarezmedinafrancisco <23308060610104@cetis61.edu.mx>
2025-11-13 00:08:07 -0700

    aaaaa

--- README.md
+++
@@ -1,3 +0,0 @@
-# equipo-juegos
-los juegos de la aplicacion web 
-\Scripts\activate
--- app.py
+++ app.py
@@ -1,37 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.secret_key = 'tu_clave_secreta_aqui'
-API = ""https://pokeapi.co/api/v2/pokemon/"
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def memorama():
 
-if __name__ == '__main__':
-    app.route(debug=True)
+    images = [
+        "static/aguacate.jpg",
+        "static/almendra.jpg", 
+        "static/arandanos.jpg",  
+        "static/avena.jpg", 
+        "static/brocoli.jpg",  
+        "static/ensalada.jpg",  
+        "static/yougurt.jpg",  
+        "static/salmon.jpg"    
+    ]
 
+    return render_template("index.html", images=images)
 
-@app.route('/pokemon',methodS =['POST'])
-def search_pokemon():
-    pokemon_name = request.form.get('pokemon')
-
-if not pokemon_name:
-    flash('por favor ingresa un nombre vlaido')
-    return redirect(url_for('index'))
-
-
-try: 
-
-
-    resp = request.get(f"{API}{pokemon_name}")
-    if resp.status_code == 200:
-    pokemon_data = resp.json()
-    return render_template('pokemon.html', pokemon)
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
